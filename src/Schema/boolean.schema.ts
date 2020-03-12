d88b59be7c8bc1d7db31f0a25e1599bad3c1e3b6
richardcrng@gmail.com <richardcrng@gmail.com>
2020-03-12 20:06:50 +0000

    Refactor primitive schema to use Object.assign

--- src/Schema/boolean.schema.ts
+++ src/Schema/boolean.schema.ts
@@ -1,25 +1,25 @@
 import { FieldOptions, FieldDefinition, FieldIdentifier } from "../types/field.types"
 
 // Overloads for required with default: i.e. it exists on document but need not be passed in
-function boolean(opts: FieldOptions<boolean> & { required: true, default: boolean }): FieldDefinition<boolean, true, true>;
-function boolean(opts: FieldOptions<boolean> & { optional: false, default: boolean }): FieldDefinition<boolean, true, true>;
-function boolean(opts: FieldOptions<boolean> & { default: boolean }): FieldDefinition<boolean, true, true>;
+function booleanFn(opts: FieldOptions<boolean> & { required: true, default: boolean }): FieldDefinition<boolean, true, true>;
+function booleanFn(opts: FieldOptions<boolean> & { optional: false, default: boolean }): FieldDefinition<boolean, true, true>;
+function booleanFn(opts: FieldOptions<boolean> & { default: boolean }): FieldDefinition<boolean, true, true>;
 
 // Overloads for required with no default: i.e. it exists on document and must be passed in
-function boolean(): FieldDefinition<boolean, true, false>
-function boolean(opts: FieldOptions<boolean>): FieldDefinition<boolean, true, false>
-function boolean(opts: FieldOptions<boolean> & { required: true }): FieldDefinition<boolean, true, false>
-function boolean(opts: FieldOptions<boolean> & { optional: false }): FieldDefinition<boolean, true, false>
+function booleanFn(): FieldDefinition<boolean, true, false>
+function booleanFn(opts: FieldOptions<boolean>): FieldDefinition<boolean, true, false>
+function booleanFn(opts: FieldOptions<boolean> & { required: true }): FieldDefinition<boolean, true, false>
+function booleanFn(opts: FieldOptions<boolean> & { optional: false }): FieldDefinition<boolean, true, false>
 
 // Overloads for optional
-function boolean(opts: FieldOptions<boolean> & { required: false }): FieldDefinition<boolean, false>;
-function boolean(opts: FieldOptions<boolean> & { optional: true }): FieldDefinition<boolean, false>;
+function booleanFn(opts: FieldOptions<boolean> & { required: false }): FieldDefinition<boolean, false>;
+function booleanFn(opts: FieldOptions<boolean> & { optional: true }): FieldDefinition<boolean, false>;
 
 // General definition
-function boolean(opts: FieldOptions<boolean> & { optional?: boolean, default?: boolean }): FieldDefinition<boolean>
-function boolean(opts: FieldOptions<boolean> & { required?: boolean, default?: boolean }): FieldDefinition<boolean>
+function booleanFn(opts: FieldOptions<boolean> & { optional?: boolean, default?: boolean }): FieldDefinition<boolean>
+function booleanFn(opts: FieldOptions<boolean> & { required?: boolean, default?: boolean }): FieldDefinition<boolean>
 
-function boolean(opts?: FieldOptions<boolean> & { required?: boolean, optional?: boolean, default?: boolean }): any {
+function booleanFn(opts?: FieldOptions<boolean> & { required?: boolean, optional?: boolean, default?: boolean }): any {
   if (!opts) return { _fieldIdentifier: FieldIdentifier.boolean, required: true }
 
   const { default: defaultVal, required, optional, ...rest } = opts
@@ -43,7 +43,6 @@ function boolean(opts?: FieldOptions<boolean> & { required?: boolean, optional?:
   return fieldConfig
 }
 
-boolean.required = true
-boolean._fieldIdentifier = FieldIdentifier.boolean
+const boolean = Object.assign(booleanFn, booleanFn())
 
 export default boolean
--- src/Schema/number.schema.ts
+++ src/Schema/number.schema.ts
@@ -1,25 +1,25 @@
 import { FieldOptions, FieldDefinition, FieldIdentifier } from "../types/field.types"
 
 // Overloads for required with default: i.e. it exists on document but need not be passed in
-function number(opts: FieldOptions<number> & { required: true, default: number }): FieldDefinition<number, true, true>;
-function number(opts: FieldOptions<number> & { optional: false, default: number }): FieldDefinition<number, true, true>;
-function number(opts: FieldOptions<number> & { default: number }): FieldDefinition<number, true, true>;
+function numberFn(opts: FieldOptions<number> & { required: true, default: number }): FieldDefinition<number, true, true>;
+function numberFn(opts: FieldOptions<number> & { optional: false, default: number }): FieldDefinition<number, true, true>;
+function numberFn(opts: FieldOptions<number> & { default: number }): FieldDefinition<number, true, true>;
 
 // Overloads for required with no default: i.e. it exists on document and must be passed in
-function number(): FieldDefinition<number, true, false>
-function number(opts: FieldOptions<number>): FieldDefinition<number, true, false>
-function number(opts: FieldOptions<number> & { required: true }): FieldDefinition<number, true, false>
-function number(opts: FieldOptions<number> & { optional: false }): FieldDefinition<number, true, false>
+function numberFn(): FieldDefinition<number, true, false>
+function numberFn(opts: FieldOptions<number>): FieldDefinition<number, true, false>
+function numberFn(opts: FieldOptions<number> & { required: true }): FieldDefinition<number, true, false>
+function numberFn(opts: FieldOptions<number> & { optional: false }): FieldDefinition<number, true, false>
 
 // Overloads for optional
-function number(opts: FieldOptions<number> & { required: false }): FieldDefinition<number, false>;
-function number(opts: FieldOptions<number> & { optional: true }): FieldDefinition<number, false>;
+function numberFn(opts: FieldOptions<number> & { required: false }): FieldDefinition<number, false>;
+function numberFn(opts: FieldOptions<number> & { optional: true }): FieldDefinition<number, false>;
 
 // General definition
-function number(opts: FieldOptions<number> & { optional?: boolean, default?: number }): FieldDefinition<number>
-function number(opts: FieldOptions<number> & { required?: boolean, default?: number }): FieldDefinition<number>
+function numberFn(opts: FieldOptions<number> & { optional?: boolean, default?: number }): FieldDefinition<number>
+function numberFn(opts: FieldOptions<number> & { required?: boolean, default?: number }): FieldDefinition<number>
 
-function number(opts?: FieldOptions<number> & { required?: boolean, optional?: boolean, default?: number }): any {
+function numberFn(opts?: FieldOptions<number> & { required?: boolean, optional?: boolean, default?: number }): any {
   if (!opts) return { _fieldIdentifier: FieldIdentifier.number, required: true }
 
   const { default: defaultVal, required, optional, ...rest } = opts
@@ -43,7 +43,6 @@ function number(opts?: FieldOptions<number> & { required?: boolean, optional?: b
   return fieldConfig
 }
 
-number.required = true
-number._fieldIdentifier = FieldIdentifier.number
+const number = Object.assign(numberFn, numberFn())
 
 export default number
