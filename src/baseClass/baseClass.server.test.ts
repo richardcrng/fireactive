61a39505abb337ad133c7c3c025b4458605bcf08
richardcrng@gmail.com <richardcrng@gmail.com>
2020-03-13 08:15:40 +0000

    Move server tests with baseClass to separate file

---
+++ src/baseClass/baseClass.server.test.ts
@@ -0,0 +1,71 @@
+import FirebaseServer from 'firebase-server'
+import baseClass from '.';
+import Schema from '../Schema';
+import initialize from '../initialize';
+
+describe('baseClass: with server connection', () => {
+  let server: FirebaseServer
+
+  beforeAll(async (done) => {
+    server = new FirebaseServer(5555, 'localhost')
+    initialize({
+      databaseURL: `ws://localhost:${server.getPort()}`
+    })
+    done()
+  })
+
+  afterAll(async (done) => {
+    await server.close()
+    done()
+  })
+  
+  describe("GIVEN a model name of 'players' and a schema", () => {
+    const className = 'player'
+    const schema = {
+      name: Schema.string,
+      age: Schema.number,
+      isCool: Schema.boolean(),
+      friends: Schema.number({ required: false }),
+      children: Schema.number({ default: 4 }),
+      parents: Schema.number({ optional: true })
+    }
+
+    describe("WHEN the model name and schema are passed to record", () => {
+      const PlayerRecord = baseClass(className, schema)
+
+      test("THEN the result is a class that can create new instances", () => {
+        const player = new PlayerRecord({ name: 'Pedro', age: 3, isCool: true })
+        expect(player.name).toBe('Pedro')
+        expect(player.age).toBe(3)
+        expect(player.isCool).toBe(true)
+        expect(player.friends).toBeUndefined()
+        expect(player.children).toBe(4)
+        expect(player.parents).toBeUndefined()
+      })
+
+      test("AND an error is thrown if required fields are not passed", () => {
+        // @ts-ignore : checking for an error
+        expect(() => new PlayerRecord({ age: 3 })).toThrow(/missing the required property/)
+      })
+
+      test("AND an error is thrown if a field is passed of the wrong type", () => {
+        // @ts-ignore : checking for an error
+        expect(() => new PlayerRecord({ name: 4, age: 3, isCool: true })).toThrow(/wrong type/)
+      })
+
+      test("AND using the model's create method throws an error without a database connection", () => {
+        expect(PlayerRecord.create({ name: 'Pedro', age: 3, isCool: true })).rejects.toThrow('Cannot get Firebase Real-Time Database instance: have you intialised the Firebase connection?')
+      })
+
+      describe("AND a connection to a database is initialised", () => {
+        test("THEN a model's create method does not throw an error", () => {
+          expect(PlayerRecord.create({ name: 'Pedro', age: 3, isCool: true })).resolves.toMatchObject({
+            name: 'Pedro',
+            age: 3,
+            isCool: true
+          })
+        })
+      })
+    })
+  })
+})
