df5a6aaeffc4959cdce2d10a7811a3fa70c46391
richardcrng@gmail.com <richardcrng@gmail.com>
2020-03-22 15:08:20 +0000

    Set to empty object for undefined indexed value

--- src/baseClass/constructor/checkPrimitive.ts
+++ src/baseClass/constructor/checkPrimitive.ts
@@ -45,6 +45,11 @@ function checkPrimitive<Schema extends RecordSchema>(this: ActiveRecord<Schema>,
         // the enum values are on the field definition's vals property
         doesMatch = schemaFieldDef.vals.includes(currentValAtPath()); break
       case FieldIdentifier.indexed:
+        const currentValue = currentValAtPath()
+        if (typeof currentValue === 'undefined') {
+          set(this, schemaKeyPath, {})
+          return true
+        }
         const currentIndexedValues = Object.values(currentValAtPath())
         doesMatch = currentIndexedValues.every(val => {
           if (typeof val === 'undefined') return true
