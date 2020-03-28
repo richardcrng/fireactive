d7fad1c617f637195e4a196218545f63b48607a4
richardcrng@gmail.com <richardcrng@gmail.com>
2020-03-28 16:50:52 +0000

    Sync record object properties to db on create

--- src/fireactive.server.test.ts
+++ src/fireactive.server.test.ts
@@ -1,46 +1,55 @@
 import { ActiveClass, Schema, initialize } from './fireactive'
 import 'dotenv/config'
 
-describe('ActiveClass', () => {
-  describe("GIVEN a model name of 'players' and a schema", () => {
-    const schema = {
-      name: Schema.string,
-      age: Schema.number,
-      isCool: Schema.boolean(),
-      friends: Schema.number({ required: false }),
-      children: Schema.number({ default: 4 }),
-      parents: Schema.number({ optional: true })
-    }
+const schema = {
+  name: Schema.string,
+  age: Schema.number,
+  isCool: Schema.boolean(),
+  friends: Schema.number({ required: false }),
+  children: Schema.number({ default: 4 }),
+  parents: Schema.number({ optional: true })
+}
 
-    describe("WHEN an `ActiveClass` is extended", () => {
-      class Player extends ActiveClass(schema) { }
+class Player extends ActiveClass(schema) {}
 
-      describe("AND a connection to a database is initialised", () => {
-        const app = initialize({
-          databaseURL: process.env.DATABASE_URL
-        })
-
-        beforeAll(async (done) => {
-          await app.database().ref().remove()
-          done()
-        })
-
-        afterAll(async (done) => {
-          await app.delete()
-          done()
-        })
-
-        test("THEN a model's create method does not throw an error", () => {
-          expect(Player.create({ name: 'Pedro', age: 3, isCool: true })).resolves.toMatchObject({
-            name: 'Pedro',
-            age: 3,
-            isCool: true
-          })
-        })
-
-      })
-    })
-  })
+const app = initialize({
+  databaseURL: process.env.DATABASE_URL
 })
 
+const db = app.database()
 
+beforeAll(async (done) => {
+  await db.ref().remove()
+  done()
+})
+
+afterAll(async (done) => {
+  await app.delete()
+  done()
+})
+
+describe('Creating an entry', () => {
+  let player: Player
+  test('Instantiating a valid `ActiveRecord`', async (done) => {
+    player = await Player.create({ name: 'Pedro', age: 3, isCool: true })
+    expect(player.name).toBe('Pedro')
+    expect(player.age).toBe(3)
+    expect(player.isCool).toBe(true)
+    expect(player.friends).toBeUndefined()
+    expect(player.children).toBe(4)
+    expect(player.parents).toBeUndefined()
+    done()
+  })
+
+  test('Checking the `ActiveRecord` against the database', async (done) => {
+    const playerSnapshot = await player.ref().once('value')
+    const playerInDb = playerSnapshot.val()
+    expect(playerInDb.name).toBe('Pedro')
+    expect(playerInDb.age).toBe(3)
+    expect(playerInDb.isCool).toBe(true)
+    expect(playerInDb.friends).toBeUndefined()
+    expect(playerInDb.children).toBe(4)
+    expect(playerInDb.parents).toBeUndefined()
+    done()
+  })
+})
