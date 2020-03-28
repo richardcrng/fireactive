22a44950672f0dc84f6aeaeb6887a86bda28196b
richardcrng@gmail.com <richardcrng@gmail.com>
2020-03-28 18:07:26 +0000

    Test reading a player by id

--- src/fireactive.server.test.ts
+++ src/fireactive.server.test.ts
@@ -28,43 +28,54 @@ afterAll(async (done) => {
   done()
 })
 
-describe('Creating an entry', () => {
+describe('CRUD', () => {
   let player: Player
-  test('Instantiating a valid `ActiveRecord`', async (done) => {
-    player = await Player.create({ name: 'Pedro', age: 3, isCool: true })
-    expect(player.name).toBe('Pedro')
-    expect(player.age).toBe(3)
-    expect(player.isCool).toBe(true)
-    expect(player.friends).toBeUndefined()
-    expect(player.children).toBe(4)
-    expect(player.parents).toBeUndefined()
-    done()
-  })
-
-  test('Checking the `ActiveRecord` against the database', async (done) => {
-    const playerSnapshot = await player.ref().once('value')
-    const playerInDb = playerSnapshot.val()
-    expect(playerInDb.name).toBe('Pedro')
-    expect(playerInDb.age).toBe(3)
-    expect(playerInDb.isCool).toBe(true)
-    expect(playerInDb.friends).toBeUndefined()
-    expect(playerInDb.children).toBe(4)
-    expect(playerInDb.parents).toBeUndefined()
-    done()
-  })
-
-  describe('syncing', () => {
-    it('fromDb is on by default', async (done) => {
-      await player.ref().update({ children: 5 })
-      expect(player.children).toBe(5)
+  describe('Creating an entry', () => {
+    test('Instantiating a valid `ActiveRecord`', async (done) => {
+      player = await Player.create({ name: 'Pedro', age: 3, isCool: true })
+      expect(player.name).toBe('Pedro')
+      expect(player.age).toBe(3)
+      expect(player.isCool).toBe(true)
+      expect(player.friends).toBeUndefined()
+      expect(player.children).toBe(4)
+      expect(player.parents).toBeUndefined()
       done()
     })
 
-    it('toDb is on by default', async (done) => {
-      player.parents = 1
-      await player.pendingSetters()
-      const parentsSnapshot = await player.ref('parents').once('value')
-      expect(parentsSnapshot.val()).toBe(1)
+    test('Checking the `ActiveRecord` against the database', async (done) => {
+      const playerSnapshot = await player.ref().once('value')
+      const playerInDb = playerSnapshot.val()
+      expect(playerInDb.name).toBe('Pedro')
+      expect(playerInDb.age).toBe(3)
+      expect(playerInDb.isCool).toBe(true)
+      expect(playerInDb.friends).toBeUndefined()
+      expect(playerInDb.children).toBe(4)
+      expect(playerInDb.parents).toBeUndefined()
+      done()
+    })
+
+    describe('syncing', () => {
+      it('fromDb is on by default', async (done) => {
+        await player.ref().update({ children: 5 })
+        expect(player.children).toBe(5)
+        done()
+      })
+
+      it('toDb is on by default', async (done) => {
+        player.parents = 1
+        await player.pendingSetters()
+        const parentsSnapshot = await player.ref('parents').once('value')
+        expect(parentsSnapshot.val()).toBe(1)
+        done()
+      })
+    })
+  })
+
+  describe("Reading an entry", () => {
+    it('can read a player by id', async (done) => {
+      expect.assertions(1)
+      const p = await Player.findById(player.getId())
+      if (p) expect(p.toObject()).toEqual(player.toObject())
       done()
     })
   })
