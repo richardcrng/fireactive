0e494eb59cd0a9bb134f113d555518907c78d988
richardcrng@gmail.com <richardcrng@gmail.com>
2020-03-30 20:31:02 +0100

    Implement alternative naming convention in relations test

--- src/ActiveClass/ActiveClass.relations.test.ts
+++ src/ActiveClass/ActiveClass.relations.test.ts
@@ -5,29 +5,40 @@ import setupTestServer from '../utils/setupTestServer';
 import '../utils/toEqSerialize'
 import ActiveClassError from './Error';
 
-describe('ActiveClass: one-to-one relation', () => {
-  const { server, db } = setupTestServer()
+const { server, db } = setupTestServer()
 
-  const playerSchema = {
-    name: Schema.string,
-    votingFor: Schema.string({ optional: true }) // player id
-  }
+const playerSchema = {
+  name: Schema.string,
+  votingForId: Schema.string({ optional: true }), // player id
+  gameId: Schema.string({ optional: true }) // game id
+}
 
-  class Player extends ActiveClass(playerSchema) {
-    async voteTarget(): Promise<Player | null> {
-      try {
-        const target = await Player.findById(this.votingFor as string)
-        return target ? target : null
-      } catch (err) {
-        return null
-      }
+const gameSchema = {
+  players: Schema.indexed.string // dict of player ids
+}
+
+class Player extends ActiveClass(playerSchema) {
+  async votingFor(): Promise<Player | null> {
+    try {
+      const target = await Player.findById(this.votingForId as string)
+      return target ? target : null
+    } catch (err) {
+      return null
     }
   }
+}
 
+class Game extends ActiveClass(gameSchema) {
+
+}
+
+
+
+describe('ActiveClass: one-to-one relation', () => {
   it('handles one-to-one relationships', async (done) => {
     const richard = await Player.create({ name: 'Richard' })
-    const rachel = await Player.create({ name: 'Rachel', votingFor: richard.getId() })
-    const target = await rachel.voteTarget()
+    const rachel = await Player.create({ name: 'Rachel', votingForId: richard.getId() })
+    const target = await rachel.votingFor()
     expect(target).toEqSerialize(richard)
     done()
   })
