f36839eb9666495b6ce5b622bc1ff87228de3ad9
richardcrng@gmail.com <richardcrng@gmail.com>
2020-03-30 20:47:21 +0100

    Test one-to-many relationship

--- src/ActiveClass/ActiveClass.relations.test.ts
+++ src/ActiveClass/ActiveClass.relations.test.ts
@@ -14,7 +14,7 @@ const playerSchema = {
 }
 
 const gameSchema = {
-  players: Schema.indexed.string // dict of player ids
+  playerIds: Schema.indexed.boolean // dict of player ids
 }
 
 class Player extends ActiveClass(playerSchema) {
@@ -29,17 +29,30 @@ class Player extends ActiveClass(playerSchema) {
 }
 
 class Game extends ActiveClass(gameSchema) {
-
+  async players(): Promise<Player[]> {
+    const promises = Object.keys(this.playerIds).map(id => Player.findById(id))
+    const res = await Promise.all(promises)
+    return res.filter(val => val) as Player[]
+  }
 }
 
+let richard: Player
+let rachel: Player
 
+test('one-to-one relationship', async (done) => {
+  richard = await Player.create({ name: 'Richard' })
+  rachel = await Player.create({ name: 'Rachel', votingForId: richard.getId() })
+  const target = await rachel.votingFor()
+  expect(target).toEqSerialize(richard)
+  done()
+})
 
-describe('ActiveClass: one-to-one relation', () => {
-  it('handles one-to-one relationships', async (done) => {
-    const richard = await Player.create({ name: 'Richard' })
-    const rachel = await Player.create({ name: 'Rachel', votingForId: richard.getId() })
-    const target = await rachel.votingFor()
-    expect(target).toEqSerialize(richard)
-    done()
-  })
+test('one-to-many relationship', async (done) => {
+  const game = await Game.create({ playerIds: {
+    [richard.getId()]: true,
+    [rachel.getId()]: true
+  }})
+  const players = await game.players()
+  expect(players).toEqSerialize([richard, rachel])
+  done()
 })
