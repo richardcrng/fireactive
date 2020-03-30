c6280f44705ac720cabc9fd0a4ea28a65715e919
richardcrng@gmail.com <richardcrng@gmail.com>
2020-03-30 20:22:36 +0100

    Write test for simple one-to-one relationship

--- src/ActiveClass/ActiveClass.relations.test.ts
+++ src/ActiveClass/ActiveClass.relations.test.ts
@@ -2,17 +2,33 @@ import ActiveClass from '.';
 import Schema from '../Schema';
 import pushWithId from '../utils/pushWithId';
 import setupTestServer from '../utils/setupTestServer';
-import '../utils/toContainObject'
+import '../utils/toEqSerialize'
 import ActiveClassError from './Error';
 
-describe('ActiveClass: with server connection', () => {
+describe('ActiveClass: one-to-one relation', () => {
   const { server, db } = setupTestServer()
 
   const playerSchema = {
     name: Schema.string,
-    votingFor: Schema.string // player id
+    votingFor: Schema.string({ optional: true }) // player id
   }
 
-  class Player extends ActiveClass(playerSchema) {}
+  class Player extends ActiveClass(playerSchema) {
+    async voteTarget(): Promise<Player | null> {
+      try {
+        const target = await Player.findById(this.votingFor as string)
+        return target ? target : null
+      } catch (err) {
+        return null
+      }
+    }
+  }
 
+  it('handles one-to-one relationships', async (done) => {
+    const richard = await Player.create({ name: 'Richard' })
+    const rachel = await Player.create({ name: 'Rachel', votingFor: richard.getId() })
+    const target = await rachel.voteTarget()
+    expect(target).toEqSerialize(richard)
+    done()
+  })
 })
