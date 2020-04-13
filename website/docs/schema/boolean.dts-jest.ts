526f4bbf43c8cfdc7784c418554efc58c971d857
richardcrng@gmail.com <richardcrng@gmail.com>
2020-04-13 11:38:43 +0100

    Write failing and passing type tests for boolean schema

--- website/docs/schema/boolean.dts-jest.ts
+++ website/docs/schema/boolean.dts-jest.ts
@@ -1,5 +1,3 @@
-// @ts-nocheck
-
 import { ActiveClass, Schema } from '../../../src'
 
 const lightbulbSchema = {
@@ -8,11 +6,26 @@ const lightbulbSchema = {
 
 class Lightbulb extends ActiveClass(lightbulbSchema) {}
 
-// @dts-jest:fail
-new Lightbulb()
+// @dts-jest:group Static errors
+{
+  // @dts-jest:fail
+  new Lightbulb()
 
-// @dts-jest:fail
-new Lightbulb({})
+  // @dts-jest:fail
+  new Lightbulb({})
 
-// @dts-jest:fail
-new Lightbulb({ isOn: 'yes' })
+  // @dts-jest:fail
+  new Lightbulb({ isOn: 'yes' })
+
+  // @dts-jest:fail
+  new Lightbulb({ isOn: null })
+}
+
+// @dts-jest:group works
+{
+  // @dts-jest:pass
+  new Lightbulb({ isOn: true })
+
+  // @dts-jest:pass
+  new Lightbulb({ isOn: false })
+}
